3bbc9d368b6bf34789c7c4e5d9b906ab902ef689
Noel Jacob <noeljacob91@gmail.com>
2024-04-24 22:19:08 +0530

    Add homepage

---
+++ webpages/homepage/README.md
@@ -0,0 +1,13 @@
+# svelte-template
+
+Simple SvelteKit template with built in Tailwind and Lucide Icons support.
+
+## Features
+
+- Dynamic sitemap.xml generation with automatic lastmod, add routes completely in JS.
+- Robots.txt set up by default.
+
+## Components
+
+- Custom Image wrapper around Unpics Image package to add type safety.
+- More coming soon...
---
+++ webpages/homepage/src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<!-- svelte-ignore missing-declaration -->
+<svelte:head>
+  <title>Soon...</title>
+  <meta
+    name="description"
+    content="SpaceHub is an open-source file manager for teams. "
+  />
+
+  <!-- OpenGraph -->
+  <meta property="og:title" content="Soon..." />
+  <meta
+    property="og:description"
+    content="SpaceHub is an open-source file manager for teams. "
+  />
+  <meta property="og:image" content="/favicon.ico" />
+  <meta property="og:url" content={`${__SITE_URL__}/`} />
+  <meta property="og:type" content="website" />
+  <meta property="og:locale" content="en_US" />
+</svelte:head>
+
+<div>
+  <i>SpaceHub</i> is an upcoming commercial open-source file manager for teams.
+  <br />
+  For any questions, please contact <a href="mailto:noel@spacehub.tech">noel@spacehub.tech</a>.
+</div>
+
+<style>
+  a {
+    color: royalblue;
+  }
+</style>
